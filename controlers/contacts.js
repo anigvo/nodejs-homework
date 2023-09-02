32f44d9a47e4bc1c1bcbb07ec1d9cbce290a86d5
anigvo <anigvo@gmail.com>
2023-09-02 18:23:08 +0300

    create task

--- controlers/contacts.js
+++ controlers/contacts.js
@@ -2,7 +2,26 @@ const { Contact } = require("../models/contacts");
 const { CreateHttpError, ctrlWrapper } = require("../helpers");
 
 const listContacts = async (req, res) => {
-  const data = await Contact.find();
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20, favorite = false } = req.query;
+  const skip = (page - 1) * limit;
+  if (favorite) {
+    const data = await Contact.find({ owner, favorite }, "-_", { skip, limit });
+    if (data.length === 0) {
+      throw CreateHttpError(
+        404,
+        "No contacts for this query or no contacts have been added"
+      );
+    }
+    return res.status(200).send(data);
+  }
+  const data = await Contact.find({ owner }, "-_", { skip, limit });
+  if (data.length === 0) {
+    throw CreateHttpError(
+      404,
+      "No contacts for this query or no contacts have been added"
+    );
+  }
   res.status(200).send(data);
 };
 
@@ -16,7 +35,8 @@ const getContactById = async (req, res) => {
 };
 
 const addContact = async (req, res) => {
-  const data = await Contact.create(req.body);
+  const { _id: owner } = req.user;
+  const data = await Contact.create({ ...req.body, owner });
   res.status(201).send(data);
 };
 
---
+++ models/users.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+const Joi = require("joi");
+const { handleMongooseError } = require("../helpers");
+const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,8})+$/;
+
+const usersSchemaMongoose = new Schema(
+  {
+    password: {
+      type: String,
+      minlength: 6,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      match: emailRegex,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const usersSchemaJoi = Joi.object({
+  password: Joi.string().min(6).required().messages({
+    "any.required": "Set password for user",
+  }),
+  email: Joi.string().pattern(emailRegex).required().messages({
+    "any.required": "Email is required",
+  }),
+  subscription: Joi.string()
+    .valid("starter", "pro", "business")
+    .default("starter"),
+  token: Joi.string(),
+});
+
+const schemas = {
+  usersSchemaJoi,
+};
+
+usersSchemaMongoose.post("save", handleMongooseError);
+
+const User = model("user", usersSchemaMongoose);
+
+module.exports = { User, schemas };
