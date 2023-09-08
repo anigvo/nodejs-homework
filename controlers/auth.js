fb5ff121c61d4a2e55632c707fdeac2149624865
anigvo <anigvo@gmail.com>
2023-09-08 19:42:48 +0300

    create test for login

--- controlers/auth.js
+++ controlers/auth.js
@@ -45,7 +45,6 @@ const login = async (req, res, next) => {
   const payload = {
     id: user._id,
   };
-  console.log(SECRET_KEY);
   const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "24h" });
   await User.findByIdAndUpdate(user._id, { token });
   res.status(200).send({
